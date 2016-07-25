fd02e5c74313d8540b3cb3245076cedc47a0059c
Jilong Liao <jilong.liao@gmail.com>
2016-07-25 14:59:57 -0700

    code

---
+++ Arduino/wifi/AzureIoT/src/util/HTTPSClient.h
@@ -0,0 +1,44 @@
+// Copyright (c) Arduino. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+#ifndef HTTPSCLIENT_H
+#define HTTPSCLIENT_H
+
+#ifdef ARDUINO_SAMD_FEATHER_M0
+#include <Adafruit_WINC1500SSLClient.h>
+#elif defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_SAMD_MKR1000)
+#include <WifiSSLClient.h>
+#elif defined(ESP8266)
+#include <WiFiClientSecure.h>
+#endif
+
+#define HTTPS_PORT          443
+
+#ifdef ARDUINO_SAMD_FEATHER_M0
+class HTTPSClient : public Adafruit_WINC1500SSLClient
+#elif defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_SAMD_MKR1000)
+class HTTPSClient : public WiFiSSLClient
+#elif defined(ESP8266)
+class HTTPSClient : public WiFiClientSecure
+#endif
+{
+    public:
+        HTTPSClient();
+        int begin(const char* host, int port = HTTPS_PORT);
+        int sendRequest(const char* method, const char* path);
+        int sendHeader(const String& header);
+        int sendBody(const unsigned char *content, int length);
+        int readStatus();
+        int readHeader(String& name, String& value);
+        size_t contentLength();
+        int readBody(unsigned char *content, int length);
+        void end();
+
+    private:
+        String readLine();
+
+        size_t _contentLength;
+};
+
+#endif
---
+++ Arduino/wifi/command_center/command_center.ino
@@ -0,0 +1,73 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <ESP8266WiFi.h>
+#include <time.h>
+#include "command_center_http.h"
+
+
+const char ssid[] = "Andy"; //  WiFi SSID (name)
+const char pass[] = "jarjarjar";    // WiFi password
+const char connectionString[] = "HostName=jar-iot-labs.azure-devices.net;DeviceId=ESP8266;SharedAccessKey=Tk9esMCeTYQnqmljNjkMyiuiPpKHKsL5Tx25RBhUaSg=";
+
+int status = WL_IDLE_STATUS;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+    initSerial();
+    initWifi();
+    initTime();
+
+    // Connected, now do something
+    run();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+  // Not used.
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void initSerial() {
+    // Start serial and initialize stdout
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void initWifi() {
+    // Attempt to connect to Wifi network:
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("Connected to wifi");
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void initTime() {
+    time_t epochTime;
+
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    while (true) {
+        epochTime = time(NULL);
+
+        if (epochTime == 0) {
+            Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
+            delay(2000);
+        } else {
+            Serial.print("Fetched NTP epoch time is: ");
+            Serial.println(epochTime);
+            break;
+        }
+    }
+}
+
