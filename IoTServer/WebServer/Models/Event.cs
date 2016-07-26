c7172acf37ee9f81541cdef0963148346332305d
Ahmed-Salama <ahmedsalama183@gmail.com>
2016-07-25 23:48:53 -0700

    IoTServer solution pulling data from Event hub

---
+++ IoTServer/WebServer/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServer.Models
+{
+    public class Event
+    {
+        public string DeviceId { get; set; }
+
+        public string WeightReading { get; set; }
+
+        public string TemperatureReading { get; set; }
+    }
+}
